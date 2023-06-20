d6470f63d8ac88514f7c49af8b4e85bac13d0e51
had499 <63800599+had499@users.noreply.github.com>
2023-06-20 16:17:13 +0100

    Fix global and implemented local alignment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from defines import SequenceAlignment
 
-test = SequenceAlignment("jjjabcd", "abcdkk","global")
-
-print(test)
+test = SequenceAlignment("what the hell is going on", "i dont what the hell is going on")
 
 print(test.reconstructGlobal())
+print(test)
